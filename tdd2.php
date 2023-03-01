6173d7a514901f351a26eac76bd286cf2a371bc6
juliengoeuriot <goeuriotj@gmail.com>
2023-03-01 16:18:35 +0100

    test CN1 echec

--- tdd2.php
+++ tdd2.php
@@ -18,7 +18,7 @@ function mention($note)
 }
 
 function testMention_CE1() 
-    { if (mention(-1) == false) 
+    { if (mention(-1) === false) 
         { 
             echo " Test mention(-1) : OK " ; 
         } 
@@ -28,4 +28,33 @@ function testMention_CE1()
         } 
     }
 testMention_CE1();
+echo PHP_EOL;
+
+function testMention_CE2() 
+    { if (mention(21) === false) 
+        { 
+            echo " Test mention(21) : OK " ; 
+        } 
+        else 
+        { 
+            echo " Test mention(21) : Échec" ;
+        } 
+    }
+    testMention_CE2();
+    echo PHP_EOL;
+
+    function testMention_CN1() 
+    { if (mention(8) === "") 
+        { 
+            echo " Test mention(8) : OK " ; 
+        } 
+        else 
+        { 
+            echo " Test mention(8) : Échec" ;
+        } 
+    }
+testMention_CN1();
+echo PHP_EOL;
+
+
 
