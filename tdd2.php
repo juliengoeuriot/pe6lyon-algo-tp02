6ca0c36b28be58c1f3af71a70ac9948c4d2a2720
Franak007 <franck.pertosa@gmail.com>
2023-03-01 15:52:43 +0100

    test1

---
+++ tdd2.php
@@ -0,0 +1,30 @@
+<?php
+
+# Attribuer une mention
+
+/*
+CE1 : mention(-1) : FAUX
+CN1 : mention(8) : ""
+CN2 : mention(12) : AB
+CN3 : mention(14) : Bien
+CN4 : mention(16) : TB
+CN5 : mention(18) : Excellent
+CE2 : mention(21) : FAUX
+*/
+
+function mention($note)
+{
+    return true ;
+}
+
+function testMention_CE1() 
+    { if (mention(-1) == false) 
+        { 
+            echo " Test mention(-1) : OK " ; 
+        } 
+        else 
+        { 
+            echo " Test mention(-1) : Échec" ;
+        } 
+    }
+testMention_CE1();
